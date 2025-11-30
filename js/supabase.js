8d96ed68180dced7ae901375ee31663182def2fc
Raul Oliveira <raulennonlima@gmail.com>
2025-11-30 12:05:19 -0300

    Fix redirect URL bug

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 // --- js/supabase.js ---
 
-const supabaseUrl = 'SUA_URL_DO_SUPABASE'; 
-const supabaseKey = 'SUA_KEY_ANON'; 
+const supabaseUrl = 'https://hyeeclvizibfzeemiqle.supabase.co'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5ZWVjbHZpemliZnplZW1pcWxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0OTQ5NDIsImV4cCI6MjA4MDA3MDk0Mn0.kE0BV4RAZweE4On2sQ3kaQWBcwa8eCcBdnwh__zDtlY'; 
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
